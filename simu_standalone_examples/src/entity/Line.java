d64f32c93b5ecd35eac8a53b663a6a923ab01d68
WernerFontana <werner.fontana@ensta-bretagne.org>
2017-01-04 14:49:03 +0100

    Rentrée des données des enums Direction et Freq Ajout getters/setters à Line

--- simu_standalone_examples/src/entity/Line.java
+++ simu_standalone_examples/src/entity/Line.java
@@ -7,8 +7,33 @@ import engine.ISimEntity;
 public class Line  implements ISimEntity {
 
 	private LinkedList<Car> cars;
-	private int longueur;
-	public final int largeur = 3;
+	private double longueur;
+	public final double largeur = 3;
 	private Node begin, end;
+	private int ID;
 	
+	public Line(int ID, double longeur, Node begin, Node end){
+		this.longueur = longueur;
+		this.begin = begin;
+		this.end = end;
+	}
+	
+	public LinkedList<Car> getCars() {
+		return cars;
+	}
+	public void setCars(LinkedList<Car> cars) {
+		this.cars = cars;
+	}
+	
+	public boolean isFull(){
+		double l = 0.0;
+		for(Car c : cars){
+			l+= c.longueur;
+			l+= c.distSecu;
+		}
+		if(l > longueur)
+			return true;
+		else
+			return false;
+	}
 }
