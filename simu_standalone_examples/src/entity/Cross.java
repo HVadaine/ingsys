d7ea45d2143b8d4aaedd9056b0b77dae0b86ab0e
OpesMentis <planchot.antoine@gmail.com>
2017-01-04 15:16:06 +0100

    Établissement du constructeur.

--- simu_standalone_examples/src/entity/Cross.java
+++ simu_standalone_examples/src/entity/Cross.java
@@ -6,9 +6,15 @@ import engine.ISimEntity;
 
 public class Cross extends Node implements ISimEntity {
 
-	Car isOccuped[] = new Car[4];
+	Car isOccupied[] = new Car[4];
 	boolean rule;
 	public final boolean STOP = true, FEU = false;
 	
-	
+	public Cross (Car car1, Car car2, Car car3, Car car4, boolean r) {
+		isOccupied[0] = car1;
+		isOccupied[1] = car2;
+		isOccupied[2] = car3;
+		isOccupied[3] = car4;
+		rule = r;
+	}
 }
